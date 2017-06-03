c86f0bd86fa16ce00f3b2e5f33344c331b14bc29
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-03 11:20:25 +0800

    Add script to create symlinks for all dotfiles in dir

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CWD=`pwd`
+
+ln -s $CWD/.zshrc $HOME/.zshrc
+ln -s $CWD/.tmux.conf $HOME/.tmux.conf
+ln -s $CWD/.vim $HOME/.vim
+ln -s $CWD/.vimrc $HOME/.vimrc
+ln -s $CWD/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
