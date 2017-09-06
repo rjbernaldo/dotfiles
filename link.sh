0e76b8e49e47d0562b28d54b1bcf184b1135baa1
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-09-06 14:07:07 +0800

    Add global eslint file

--- link.sh
+++ link.sh
@@ -8,3 +8,4 @@ ln -sf $CWD/.vim $HOME/.vim
 ln -sf $CWD/.vimrc $HOME/.vimrc
 ln -sf $CWD/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 ln -sf $CWD/Franz/Plugins $HOME/Library/Application\ Support/Franz
+ln -sf $CWD/.eslintrc $HOME/.eslintrc
