79112d0ea7a6ccd53eebd2905e0646ddcdcf7bf3
Rj Bernaldo <rj.bernaldo@gmail.com>
2018-04-30 08:59:49 +1000

    Add config for all directions

--- .slate.js
+++ .slate.js
@@ -11,17 +11,26 @@ const fullscreen = slate.operation('move', {
 const left = slate.operation('move', {
   x: `screenOriginX+screenSizeX*${gutter}/100/2`,
   y: `screenOriginY+screenSizeY*${gutter}/100/2`,
-  width: `((screenSizeX-screenSizeX*${gutter}/100)/2)-(screenSizeX*${gutter}/100/2)/2/2`,
+  width: `((screenSizeX-screenSizeX*${gutter}/100)/2)-(screenSizeX*${gutter}/100/2)/2`,
   height: `(screenSizeY-screenSizeY*${gutter}/100)-screenSizeY*${gutter}/100/2`,
 });
 
 const right = slate.operation('move', {
-  x: `(screenSizeX/2)+screenSizeX*${gutter}/100/2`,
+  x: `(screenSizeX/2)+screenSizeX*${gutter}/100/2/2`,
   y: `screenOriginY+screenSizeY*${gutter}/100/2`,
-  width: `((screenSizeX-screenSizeX*${gutter}/100)/2)-(screenSizeX*${gutter}/100/2)/2/2`,
+  width: `((screenSizeX-screenSizeX*${gutter}/100)/2)-(screenSizeX*${gutter}/100/2)/2`,
   height: `(screenSizeY-screenSizeY*${gutter}/100)-screenSizeY*${gutter}/100/2`,
+});
+
+const down = slate.operation('move', {
+  x: `(screenSizeX/2)+screenSizeX*${gutter}/100/2/2`,
+  y: `(screenSizeY/2)+screenSizeY*${gutter}/100`,
+  width: `((screenSizeX-screenSizeX*${gutter}/100)/2)-(screenSizeX*${gutter}/100/2)/2`,
+  height: `((screenSizeY-screenSizeY*${gutter}/100)/2)-(screenSizeY*${gutter}/100)/2`,
+  //height: `((screenSizeY-screenSizeY*${gutter}/100)/2)-(screenSizeY*${gutter}/100/2)/2/2`,
 });
 
 slate.bind('k' + shortcut, (win) => win.doOperation(fullscreen));
 slate.bind('h' + shortcut, (win) => win.doOperation(left));
 slate.bind('l' + shortcut, (win) => win.doOperation(right));
+slate.bind('j' + shortcut, (win) => win.doOperation(down));
